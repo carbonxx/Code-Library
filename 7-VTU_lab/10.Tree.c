5bf97a3f8ac5278f926e7665e9d5d49f2a33837e
C.a.r.b.O.n.X <vishnunv142000@gmail.com>
2021-04-14 22:25:22 +0530

    error fixed

--- 7-VTU_lab/10.Tree.c
+++ 7-VTU_lab/10.Tree.c
@@ -4,121 +4,123 @@ a. Create a BST of N Integers: 6, 9, 5, 2, 8, 15, 24, 14, 7, 8, 5, 2
 b. Traverse the BST in Inorder, Preorder and Post Order
 c. Search the BST for a given element (KEY) and report the appropriate message
 e. Exit*/
-
 #include<stdio.h>
 #include<stdlib.h>
-typedef struct tree* treeptr;
-typedef struct tree{
+#include<malloc.h>
+struct bst
+{
     int data;
-    treeptr llink;
-    treeptr rlink;
-}tree;
-int f=0;
-void inOrder(treeptr root){
-    if(root){
-        inOrder(root->llink);
-        printf("%d ",root->data);
-        inOrder(root->rlink);
+    struct bst *lc,*rc;
+};
+typedef struct bst node;
+
+node *create(node *root,int a)
+{
+    node *temp;
+    if(root ==NULL)
+    {
+        temp=(node*) malloc(sizeof(node));
+        root =temp;
+        temp->data=a;
+        temp->rc=temp->lc=NULL;
+        return root;
+    }
+    else if(a<root->data) root->lc=create(root->lc,a);
+    else if(a>root->data) root->rc=create(root->rc,a);
+    else return root;
+}
+ void search(node *root,int k)
+ {
+     if(root==NULL) printf("Key not found\n");
+     else if(k<root->data) search(root->lc,k);
+     else if(k>root->data) search(root->rc,k);
+     else printf("Key is found\n");
+}
+void preorder(node *root)
+{
+    if(root!=NULL)
+    {
+        printf("%d\t",root->data);
+        preorder(root->lc);
+        preorder(root->rc);
     }
 }
-void preOrder(treeptr root){
-    if(root){
-        printf("%d ",root->data);
-        preOrder(root->llink);
-        preOrder(root->rlink);
+void postorder(node *root)
+{
+    if(root!=NULL)
+    {
+
+        postorder(root->lc);
+        postorder(root->rc);
+        printf("%d\t",root->data);
     }
 }
-void postOrder(treeptr root){
-    if(root){
-        postOrder(root->llink);
-        postOrder(root->rlink);
-        printf("%d ",root->data);
+void inorder(node *root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->lc);
+        printf("%d\t",root->data);
+        inorder(root->rc);
     }
 }
-void search(treeptr root,int data){
-    if(root){
-        if(data<root->data)
-            search(root->llink,data);
-        else if(data>root->data)
-            search(root->rlink,data);
-        else{
-            f=1;
-            return;
-        }
-    }
-}
-treeptr newNode(int data){
-    treeptr temp=(treeptr)malloc(sizeof(*temp));
-    temp->data=data;
-    temp->llink=temp->rlink=NULL;
-    return temp;
-}
-treeptr insert(treeptr root,int data){
-    if(!root)
-        return newNode(data);
-    if(data<root->data)
-        root->llink=insert(root->llink,data);
-    if(data>root->data)
-        root->rlink=insert(root->rlink,data);
-    return root;
-}
-treeptr findMin(treeptr root){
-    while(root->llink!=NULL)
-        root=root->llink;
-    return root;
-}
-treeptr delete(treeptr root,int data){
-    if(!root)
-        return NULL;
-    if(root->data==data){
-        if((!root->llink)&&(!root->rlink))
-            return NULL;
-        else if(root->llink&&!root->rlink)
-            return root->llink;
-        else if(root->rlink&&!root->llink)
-            return root->rlink;
-        else{
-            int min=findMin(root->rlink)->data;
-            root->data=min;
-            root->rlink=delete(root->rlink,min);
-        }
-    }
-    else if(data<root->data)
-        root->llink=delete(root->llink,data);
-    else
-        root->rlink=delete(root->rlink,data);
-    return root;
-}
-int main(){
-    treeptr root=NULL;
-    int a[]={6,9,5,2,8,15,24,14,7,8,5,2};
-    int i,n,ch;
-    for(i=0;i<12;i++)
-        root=insert(root,a[i]);
-    printf("\nMENU:\n1.Inorder\n2.PreOrder\n3.PostOrder\n4.Search\n5.Delete\n6.Exit");
-    while(1){
-        printf("\nEnter your choice:\n");
+int traverse(node *root)
+{
+    int ch;
+    while(1)
+    {
+        printf("\n---MENU---\n");
+        printf("1. Inorder traversal\n2. Preorder traversal\n3. Postorder traversal\n4. exit\n");
+        printf("Enter the option\n");
         scanf("%d",&ch);
-        switch(ch){
-            case 1: inOrder(root);
-                    break;
-            case 2: preOrder(root);
-                    break;
-            case 3: postOrder(root);
-                    break;
-            case 4: printf("\nEnter element to be searched:\n");
-                    scanf("%d",&n);
-                    search(root,n);
-                    if(f==0)
-                        printf("\nElement not found\n");
-                    else
-                        printf("\nElement found\n");
-                    break;
-            case 5: printf("\nEnter element to be deleted\n");
-                    scanf("%d",&n);
-                    root=delete(root,n);
-                    break;
-            case 6: exit(0);
+        switch (ch)
+        {
+        case 1:printf("Inorder traversal\n");
+                inorder(root);
+                break;
+        case 2:printf("Preorder traversal\n");
+                preorder(root);
+                break;
+        case 3:printf("Postorder traversal\n");
+                postorder(root);
+                break;
+        default:return 0;
+
         }
     }
+}
+void main()
+{
+    node *root;
+    int i,n,ele,key,ch;
+    root=NULL;
+    while (1)
+    {
+        printf("\n---MENu---\n");
+        printf("1. create\n2. search\n3. traversal\n4. exit\n");
+        printf("Enter the option\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:printf("Enter the number of elements\n");
+                    scanf("%d",&n);
+                    printf("Enter the elements to be added\n");
+                    for(i=0;i<n;i++)
+                    {
+                         scanf("%d",&ele);
+                         root=create(root,ele);
+                    }
+                    break;
+            case 2:printf("Enter the element to be searched\n");
+                    scanf("%d",&key);
+                    search(root,key);
+                    break;
+            case 3:traverse(root);
+                    break;
+            case 4:exit(0);
+            default:printf("Invalid option\n");
+
+        }
+    }
+
 }
