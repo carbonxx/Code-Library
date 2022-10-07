6e29da105ac6d7b7af9e2a497bced9093001f0ff
Deveesh Shetty <deveeshshetty@gmail.com>
2022-10-07 21:46:09 +0530

    Added Some Pattern Programs

---
+++ Programs/cpp/Patterns/Rectangle.cpp
@@ -0,0 +1,17 @@
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include <iostream>
+
+int main() {
+    int n = 5;
+    for (int row = 1; row <= n; row++) {
+        for (int col = 1; col <= n; col++) {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+}
