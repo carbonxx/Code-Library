38700d8b2dae894d0b01ddf0acb6a2de8c73393a
Likit.D <101191538+li153@users.noreply.github.com>
2022-10-29 08:00:11 +0530

    Update RightAngleTriangle.cpp
    this is an inward right angle triangle

--- Programs/cpp/Patterns/RightAngleTriangle.cpp
+++ Programs/cpp/Patterns/RightAngleTriangle.cpp
@@ -1,19 +1,19 @@
-// *
-// **
-// ***
-// ****
-
+// C++ program to print inverted
+// right triangle
 #include <iostream>
+using namespace std;
 
-int main() {
-    int n = 4;
-    for (int row = 1; row <= n; row++) {  // Here 4 bcoz number of lines is 4
-        // for every row run no. of columns
-        // Here no. of columns in each row is equal to the row number.
-        for (int col = 1; col <= row; col++) {
-            std::cout << "* ";
-        }
-        // adding newline when a row is printed
-        std::cout << std::endl;
-    }
+// Driver code
+int main()
+{
+int n = 5;
+
+// ith row has n-i+1 elements
+for(int i = 1; i <= n; i++)
+{
+	for(int j = 1; j <= n - i + 1; j++)
+	cout << "* ";
+	cout << endl;
+}
+return 0;
 }
